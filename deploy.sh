a2f85e1a2439cfd8cc1ea87bef447b46e89d722f
范邦雨 <fanbangyu@fanbangyudeMacBook-Pro.local>
2020-01-17 15:48:17 +0800

    添加docker-compose启动依赖，便于本地开发调试

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ docker stop $server_name && docker rm $server_name
 
 # docker pull abang/$server_name:$tag
 
-docker run -d --network goWechat --name $server_name -p 10086:80 abang/$server_name:$tag 
+docker run -d --network wechat_goWechat --name $server_name -p 10086:80 abang/$server_name:$tag 
 
 docker logs -f --tail 20 $server_name
