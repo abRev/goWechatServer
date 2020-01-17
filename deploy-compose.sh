4e8c572c50072e908660cf5bc737b74ca42979fd
范邦雨 <fanbangyu@fanbangyudeMacBook-Pro.local>
2020-01-17 17:01:35 +0800

    修改启动配置

---
+++ deploy-compose.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 通过compose启动服务,先编译服务，在使用docker打包镜像
+docker-compose stop
+docker-compose rm
+docker rm postgres redis go_wechat_server_compose
+sh build.sh compose
+docker-compose up -d
